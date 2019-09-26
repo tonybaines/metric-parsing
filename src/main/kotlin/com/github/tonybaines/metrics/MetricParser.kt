6f5bf9c98f8c6bdde61bbfd918719fe37c76ca75
Tony Baines <tony.baines.1@gmail.com>
2019-09-26 18:38:21 +0100

    Sad path handling Accidentally passing acceptance test (non-specific assertion)

--- src/main/kotlin/com/github/tonybaines/metrics/MetricParser.kt
+++ src/main/kotlin/com/github/tonybaines/metrics/MetricParser.kt
@@ -1,22 +1,28 @@
 package com.github.tonybaines.metrics
 
+import io.vavr.control.Try
+import io.vavr.kotlin.`try`
 import java.io.File
 import java.time.Instant
 
-private val String.isLong: Boolean
-    get() = this.toLongOrNull() != null
-
 sealed class MetricRecord() {
     companion object {
-        fun from(fields: List<String>): MetricRecord =
-            BasicGraphiteMetric(fields[0], Value.from(fields[1]), fields[2].toInstant())
+        fun from(fields: List<String>): Try<out MetricRecord> =
+            BasicGraphiteMetric.from(fields)
     }
-    data class BasicGraphiteMetric(val id: String, val value: Value, val timestamp: Instant): MetricRecord() {
+
+    data class BasicGraphiteMetric(val id: String, val value: Value, val timestamp: Instant) : MetricRecord() {
+        companion object {
+            fun from(fields: List<String>): Try<BasicGraphiteMetric> =
+                `try` { BasicGraphiteMetric(fields[0], Value.from(fields[1]), fields[2].toInstant()) }
+        }
+
         override fun toString(): String = "[$timestamp : '$id' = '$value']"
     }
+
 }
 
-private fun String.toInstant(): Instant  {
+private fun String.toInstant(): Instant {
     val epochTime = this.toLong()
     return if (epochTime > 2000000000) {
         Instant.ofEpochMilli(epochTime)
@@ -27,19 +33,24 @@ private fun String.toInstant(): Instant  {
 
 sealed class Value {
     companion object {
-        fun from(i: Long): Value = LongValue(i)
-        fun from(f: String): Value = when {
-            f.isLong -> LongValue(f.toLong())
-            else -> FloatValue(f.toFloat())
+        fun from(l: Long): Value = LongValue(l)
+        fun from(s: String): Value = when {
+            s.isLong -> LongValue(s.toLong())
+            else -> DoubleValue(s.toDouble())
         }
+
+        private val String.isLong: Boolean
+            get() = this.toLongOrNull() != null
     }
 
     data class LongValue(val value: Long) : Value() {
         override fun toString(): String = value.toString()
     }
-    data class FloatValue(val value: Float) : Value(){
+
+    data class DoubleValue(val value: Double) : Value() {
         override fun toString(): String = value.toString()
     }
+
 }
 
 class MetricParser(private val input: List<String>) {
@@ -48,7 +59,9 @@ class MetricParser(private val input: List<String>) {
     }
 
     fun validRecords(): List<MetricRecord> = input
-        .map{it.split(' ')}
-        .map { MetricRecord.from(it) }
+        .map { it.split(' ') }
+        .map { fields -> MetricRecord.from(fields) }
+        .filter { attempt -> attempt.isSuccess }
+        .map { successful -> successful.get() }
 
 }
